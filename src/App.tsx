a31be8d290d0dda288a704467840c11c86b1aa98
maeda <takanori@stroly.jp>
2019-10-07 23:07:38 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 import './App.css';
 
 const App: React.FC = () => {
+  React.useEffect(() => {
+    firebase.initializeApp({
+      apiKey: process.env.REACT_APP_API_KEY,
+      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+      projectId: process.env.REACT_APP_PROJECT_ID,
+    });
+
+    const db = firebase.firestore();
+
+    db.collection("comments").add({
+      name: "Ada",
+      comment: "Lovelace",
+    })
+      .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+
+    db.collection('comments').get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+      });
+    });
+  });
+
+  function handleClick() {
+    console.log('aaa');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>仁保島村 管理画面</h1>
+
+      <h2>来館者の声登録</h2>
+      <div>
+        <input type="text" placeholder="来館者名" />
+      </div>
+      <div>
+        <textarea placeholder="来館者の声"></textarea>
+      </div>
+      <div>
+        <button onClick={() => handleClick()}>登録</button>
+      </div>
     </div>
   );
 }
