f19ac0c986b2b989d6c9cd4d20d3653764dfde0e
maeda <takanori@stroly.jp>
2020-01-14 15:59:49 +0900

    create & read 処理修正

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import 'firebase/firestore';
 
 import './App.css';
 
+interface Message {
+  name: string;
+  comment: string;
+}
+
 const App: React.FC = () => {
+  const [messages, setMessages] = React.useState<Message[]>([]);
+  const [db, setDb] = React.useState<firebase.firestore.Firestore>();
+
   React.useEffect(() => {
     firebase.initializeApp({
       apiKey: process.env.REACT_APP_API_KEY,
@@ -12,46 +20,104 @@ const App: React.FC = () => {
       projectId: process.env.REACT_APP_PROJECT_ID,
     });
 
-    const db = firebase.firestore();
+    setDb(firebase.firestore());
+  }, []);
 
-    db.collection("comments").add({
-      name: "Ada",
-      comment: "Lovelace",
-    })
-      .then(function (docRef) {
-        console.log("Document written with ID: ", docRef.id);
-      })
-      .catch(function (error) {
-        console.error("Error adding document: ", error);
-      });
+  // const db = firebase.firestore();
+  React.useEffect(() => {
+    getMessages();
+  }, [db]);
 
-    db.collection('comments').get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-      });
-    });
-  });
+  const [name, setName] = React.useState('');
+  function handleNameChange(event: React.FormEvent<HTMLInputElement>) {
+    setName(event.currentTarget.value);
+  }
+
+  const [comment, setComment] = React.useState('');
+  function handleCommentChange(event: React.FormEvent<HTMLInputElement>) {
+    setComment(event.currentTarget.value);
+  }
 
   function handleClick() {
-    console.log('aaa');
+    if (!db) {
+      return;
+    }
+    if (name && comment) {
+      const message = {
+        name,
+        comment,
+      };
+      addMessage(message);
+      getMessages();
+      setName('');
+      setComment('');
+    }
+  }
+
+  function addMessage(message: Message) {
+    if (!db) {
+      return;
+    }
+    db.collection('comments')
+      .add(message)
+      .then(docRef => {
+        console.log('Document written with ID: ', docRef.id);
+      })
+      .catch(error => {
+        console.error('Error adding document: ', error);
+      });
+  }
+
+  function getMessages() {
+    if (!db) {
+      return;
+    }
+    db.collection('comments')
+      .get()
+      .then(querySnapshot => {
+        let newMessages: any[] = [];
+        querySnapshot.forEach(doc => {
+          newMessages = [...newMessages, doc.data()];
+        });
+
+        setMessages(newMessages);
+      });
   }
 
   return (
     <div className="App">
-      <h1>仁保島村 管理画面</h1>
+      <h1>React Firebase Sample</h1>
 
-      <h2>来館者の声登録</h2>
       <div>
-        <input type="text" placeholder="来館者名" />
+        <input
+          type="text"
+          placeholder="Name"
+          defaultValue={name}
+          onChange={event => handleNameChange(event)}
+        />
+        :{' '}
+        <input
+          type="text"
+          placeholder="Comment"
+          defaultValue={comment}
+          onChange={event => handleCommentChange(event)}
+        />
+        <button onClick={() => handleClick()}>Add</button>
       </div>
-      <div>
-        <textarea placeholder="来館者の声"></textarea>
-      </div>
-      <div>
-        <button onClick={() => handleClick()}>登録</button>
-      </div>
+
+      <ul>
+        {messages.map((message, index) => {
+          return (
+            <li key={index}>
+              <p>
+                {message.name}: {message.comment}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
